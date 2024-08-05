3ce04dc6c093a0a532b82a54c2ba99cb58b5d64a
ìtjdbsg <jsy00319@gmail.com>
2024-08-05 17:51:43 +0900

    work

---
+++ src/main/java/com/ll/sapp/SappApplication.java
@@ -0,0 +1,13 @@
+package com.ll.sapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/sapp/SappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
