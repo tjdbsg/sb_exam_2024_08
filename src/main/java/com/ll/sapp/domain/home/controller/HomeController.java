e058c63f5cd3d9aecae2ae5667baaf5d7dc647c7
ìtjdbsg <jsy00319@gmail.com>
2024-08-06 10:11:59 +0900

    work

---
+++ src/main/java/com/ll/sapp/domain/home/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.ll.sapp.domain.home.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String showMain() {
+        return "home/main";
+    }
+
+}
---
+++ src/main/java/com/ll/sapp/global/security/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.ll.sapp.global.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests
+                .requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+                .csrf((csrf) -> csrf.ignoringRequestMatchers(new AntPathRequestMatcher("h2-consol/**")))
+                .headers((headers) -> headers.addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN)))
+                .formLogin((formLogin) -> formLogin.loginPage("/member/login").defaultSuccessUrl("/"))
+                .logout((logout) -> logout.logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+                        .logoutSuccessUrl("/").invalidateHttpSession(true))
+                .oauth2Login(
+                        oauth2Login -> oauth2Login
+                                .loginPage("/member.login")
+                );
+        return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
